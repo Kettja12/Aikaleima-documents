4c4eb82989b1c9640353f5148063802408affe87
Jari Kettunen <jari.kettunen@proton.me>
2025-10-16 07:24:35 +0300

    *

---
+++ js/IndexedDbAccessor.js
@@ -0,0 +1,47 @@
+﻿export function initialize()
+{
+    let yasIndexedDb = indexedDB.open(DATABASE_NAME, CURRENT_VERSION);
+    yasIndexedDb.onupgradeneeded = function ()
+    {
+        let db = yasIndexedDb.result;
+        db.createObjectStore("settings", { keyPath: "key" });
+    }
+}
+
+export function set(collectionName, value)
+{
+    let yasIndexedDb = indexedDB.open(DATABASE_NAME, CURRENT_VERSION);
+
+    yasIndexedDb.onsuccess = function ()
+    {
+        let transaction = yasIndexedDb.result.transaction(collectionName, "readwrite");
+        let collection = transaction.objectStore(collectionName)
+        collection.put(value);
+    }
+}
+
+export async function get(collectionName, id)
+{
+    let request = new Promise((resolve) =>
+    {
+        let yasIndexedDb = indexedDB.open(DATABASE_NAME, CURRENT_VERSION);
+        yasIndexedDb.onsuccess = function ()
+        {
+            let transaction = yasIndexedDb.result.transaction(collectionName, "readonly");
+            let collection = transaction.objectStore(collectionName);
+            let result = collection.get(id);
+
+            result.onsuccess = function (e)
+            {
+                resolve(result.result);
+            }
+        }
+    });
+
+    let result = await request;
+
+    return result;
+}
+
+let CURRENT_VERSION = 2;
+let DATABASE_NAME = "YAS";
---
+++ js/downloadFile.js
@@ -0,0 +1,9 @@
+﻿function downloadFile(filename, base64) {
+    // Create a temporary anchor element and trigger download of a base64 data URL
+    const link = document.createElement('a');
+    link.href = 'data:text/csv;base64,' + base64;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+}
