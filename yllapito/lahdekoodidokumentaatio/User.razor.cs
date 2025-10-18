c780a36b9372abc9ca3fd392d6f9ed0389d057a7
Jari Kettunen <jari.kettunen@proton.me>
2025-10-18 19:23:12 +0300

    *

---
+++ yllapito/lahdekoodidokumentaatio/User.razor.cs
@@ -0,0 +1,25 @@
+﻿@code {
+	[Inject] State state { get; set; } = default!;
+
+	[Parameter] public int UserId { get; set; }
+	[Parameter] public EventCallback<int> UserChangedAsync { get; set; }
+
+	protected override async Task OnParametersSetAsync()
+	{
+		await state.UserModel.SelectUserAsync(UserId);
+	}
+	public async Task SaveUser()
+	{
+		await state.UserModel.SaveUser();
+		if (UserId != state.UserModel.User.Id)
+		{
+			await UserChangedAsync.InvokeAsync(state.UserModel.User.Id);
+		}
+	}
+	public async Task SendRegistrationKey()
+	{
+		await state.UserModel.SendRegistrationKey();
+	}
+
+
+}
---
+++ yllapito/lahdekoodidokumentaatio/UserModel.cs
@@ -0,0 +1,72 @@
+﻿using Shared;
+public partial class State
+{
+    public UserResponse RequestedUser { get; set; } = new();
+
+    public UserResponse User { get; set; } = new();
+
+    UserModel? userModel;
+    public UserModel UserModel
+    {
+        get
+        {
+            if (userModel == null) userModel = new(this);
+            return userModel;
+        }
+    }
+
+}
+
+public class UserModel
+{
+    public UserResponse User = new();
+
+    State state;
+    public UserModel(State state)
+    {
+        this.state = state;
+    }
+
+    public async Task SelectUserAsync(int id)
+    {
+        IdRequest request = new(state.LoginToken, id);
+        User = await state.Services.UserAsync(request);
+    }
+
+    public string SendRegistrationKeyDisabled
+    {
+        get { return User.Id == 0?"True":"False";}
+    }
+
+    public async Task SendRegistrationKey()
+    {
+        await SaveUser();
+        SendUserRegistrationRequest request = new(state.LoginToken, User);
+        var response = await state.Services.SendUserRegistrationAsync(request);
+        User = response.User;
+    }
+    public void AddUser()
+    {
+        User = new();
+    }
+    public async Task SaveUser()
+    {
+        UserSaveRequest request = new(state.LoginToken, User);
+        User = await state.Services.SaveUserAsync(request);
+        if (User.Status == "")
+        {
+            state.MessageType = MessageModel.AlertInfo;
+            state.MessageContent = Shared.ResponseMessages.SaveSuccess;
+            state.Refresh++;
+        }
+
+        if (User.Status != "")
+        {
+            state.MessageType = MessageModel.AlertWarning;
+            state.MessageContent = User.Status;
+            state.Refresh++;
+        }
+    }
+
+}
+
