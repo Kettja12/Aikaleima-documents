3b91f6c0cbc6a8db995d5554d6b2eb77c6a188f5
Jari Kettunen <jari.kettunen@proton.me>
2025-10-19 15:36:09 +0300

    *

---
+++ yllapito/lahdekoodidokumentaatio/domain/State.cs
@@ -0,0 +1,28 @@
+﻿using Shared;
+
+public partial class State
+{
+    public Services Services;
+    public State(Services services)
+    {
+        this.Services = services;
+    }
+    public bool Loaded { get; set; }
+    public string LoginToken
+    {
+        get
+        {
+            if (User.Token != "") return User.Token;
+            if (Device.LoginToken != "") return Device.LoginToken;
+            return "";
+        }
+
+    }
+
+    public string MessageType { get; set; } = "";
+    public string MessageContent { get; set; } = "";
+    public int Refresh { get; set; } = 0;
+
+
+
+}
